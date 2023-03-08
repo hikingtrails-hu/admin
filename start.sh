394d0e47f56d591ef7e8a9bd4451f74eead841fd
Lencse <leventeloki@gmail.com>
2023-03-08 23:21:13 +0100

    .

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 
-echo $GCLOUD_RUNTIME_SERVICE_ACCOUNT | base64 --decode > hikingtrails-runtime-service-account-key.json
+set -ex
+
+echo "$GCLOUD_RUNTIME_SERVICE_ACCOUNT" | base64 --decode > hikingtrails-runtime-service-account-key.json
 dumb-init ./node_modules/.bin/remix-serve build
