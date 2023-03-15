92a5c07ef279d9d7acd39141b102e19ae8294e41
Lencse <leventeloki@gmail.com>
2023-03-15 21:54:52 +0100

    .

--- src/components/Navbars/AdminNavbar.tsx
+++ src/components/Navbars/AdminNavbar.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import UserDropdown from '~/components/Dropdowns/UserDropdown'
+import {Form} from "@remix-run/react";
 
 export default function AdminNavbar() {
     return (
@@ -9,17 +10,35 @@ export default function AdminNavbar() {
             <nav className="absolute top-0 left-0 w-full z-10 bg-transparent md:flex-row md:flex-nowrap md:justify-start flex items-center p-4">
                 <div className="w-full mx-autp items-center flex justify-between md:flex-nowrap flex-wrap md:px-10 px-4">
                     {/* Brand */}
-                    <a
-                        className="text-white text-sm uppercase hidden lg:inline-block font-semibold"
-                        href="#pablo"
-                        onClick={(e) => e.preventDefault()}
-                    >
-                        Dashboard
-                    </a>
-                    {/* User */}
-                    <ul className="flex-col md:flex-row list-none items-center hidden md:flex">
-                        <UserDropdown />
+                    <ul className='flex'>
+                        <li className='mr-6'>
+                            <a
+                                className="text-white text-sm uppercase hidden lg:inline-block font-semibold"
+                                href="/"
+                            >
+                                Dashboard
+                            </a>
+                        </li>
+                        <li className='mr-6'>
+                            <a
+                                className="text-white text-sm uppercase hidden lg:inline-block font-semibold"
+                                href="/bluetrail-data"
+                            >
+                                Blue Trail Data
+                            </a>
+                        </li>
                     </ul>
+                    {/* User */}
+                    <div className="text-white text-sm uppercase">
+                        <Form action='/logout' method='post'>
+                            <button type='submit' >
+                                Logout
+                                <span className="ml-2 align-text-top">
+                                    <i className="fa-solid fa-person-through-window"></i>
+                                </span>
+                            </button>
+                        </Form>
+                    </div>
                 </div>
             </nav>
             {/* End Navbar */}
---
+++ src/routes/logout/index.tsx
@@ -0,0 +1,24 @@
+import { ActionArgs, json, redirect, Response } from '@remix-run/node'
+import registerBg from '~/assets/img/register_bg_2.png'
+import Login from '~/views/auth/Login'
+import { storage } from '~/lib/storage'
+import { User } from '~/user/user'
+import { verify } from 'argon2'
+import invariant from 'tiny-invariant'
+import { sign } from 'jsonwebtoken'
+import { config } from '~/lib/config/config'
+import { serialize } from 'cookie'
+
+export const action = async (args: ActionArgs) => {
+    return redirect('/', {
+        headers: {
+            'Set-Cookie': serialize('token', null, {
+                expires: new Date('Thu, 01 Jan 1970 00:00:00 GMT'),
+                path: '/',
+                httpOnly: true,
+                secure: true,
+                sameSite: 'strict'
+            }),
+        },
+    })
+}
