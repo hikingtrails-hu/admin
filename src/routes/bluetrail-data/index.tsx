06d6616b01b6546929a14354a2a0a7a52dc1e1ed
Lencse <leventeloki@gmail.com>
2023-03-16 01:18:48 +0100

    .

--- src/routes/bluetrail-data/index.tsx
+++ src/routes/bluetrail-data/index.tsx
@@ -40,7 +40,16 @@ const Index = () => {
             <div className="relative bg-blueGray-100">
                 <AdminNavbar />
                 {/* Header */}
-                <HeaderStats />
+                <div className="relative bg-lightBlue-600 md:pt-32 pb-32 pt-12">
+                    <div className="flex flex-wrap">
+                        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
+                            x
+                        </div>
+                        <div className="w-full xl:w-4/12 px-4">
+                            b
+                        </div>
+                    </div>
+                </div>
                 <div className="px-4 md:px-10 mx-auto w-full -m-24">
                     {/*<Switch>*/}
                     {/*    <Route path="/admin/dashboard" exact component={Dashboard} />*/}
