ba2ae6e224d980a302e4d25b7a8e937a9abdf7f7
Lencse <leventeloki@gmail.com>
2023-03-25 22:18:42 +0100

    .

--- src/components/map/Selector.tsx
+++ src/components/map/Selector.tsx
@@ -1,15 +1,16 @@
 import {Trail} from "~/core/types/types";
+import {sprintf} from "sprintf-js";
 import React from "react";
+import {formatDistance} from "~/lib/map/map";
 
 export const TrailSelector = (props: { trail: Trail }) => {
     const {trail} = props
-    const [opened, setOpened] = React.useState(false)
+    const [opened, setOpened] = React.useState(true)
     return <li key={trail.id}>
         <h2 className='border-t border-solid'>
             <button
-                className='p-4 text-xs font-bold text-blueGray-600  p-4 block w-full text-left'
+                className='p-4 font-bold text-blueGray-600  p-4 block w-full text-left relative'
                 onClick={() => {
-                    console.log(123)
                     setOpened(!opened)
                 }}
             >
@@ -19,20 +20,43 @@ export const TrailSelector = (props: { trail: Trail }) => {
                 </span>
             </button>
         </h2>
-        {/*<ul>*/}
-        {/*    {trail.path.checkpoints.map((checkpoint, i) => (*/}
-        {/*        <li key={i}>*/}
-        {/*            <h3 className='p-1 ml-3 font-bold text-blueGray-600'>{checkpoint.name}</h3>*/}
-        {/*        </li>*/}
-        {/*    ))}*/}
-        {/*</ul>*/}
+        { opened ?
+        <ul className='border-t border-solid'>
+            {trail.path.checkpoints.map((checkpoint, i) => (
+                <li key={i}>
+                    <h3 className='p-4 text-blueGray-600  p-4 block w-full text-left'>
+                        {i+1}. {checkpoint.name}
+                    </h3>
+                    <ul className='bg-white'>
+                        {checkpoint.locations.map((location, j) => (
+                            <li key={j}>
+                                <button
+                                    className='p-4 text-blueGray-600 p-4 block w-full text-left relative'
+                                    onClick={() => {
+                                        console.log(123)
+                                    }}
+                                >
+                                    <span className='block mr-4'>
+                                    {location.description}
+
+                                    </span>
+                                    <span className='absolute right-4 top-4'>
+                    <i className='fa-solid fa-bullseye'/>
+                </span>
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                </li>
+            ))}
+        </ul> : null }
     </li>
 }
 
 export const Selector = (props: {trails: Trail[]}) => {
     const {trails} = props
     return (
-        <ul className='border-b border-solid'>
+        <ul className='border-b border-solid text-xs'>
             {trails.map(trail => (
                 <TrailSelector key={trail.id} trail={trail} />
             ))}
---
+++ src/lib/map/map.ts
@@ -0,0 +1,4 @@
+import {sprintf} from "sprintf-js";
+
+export const formatDistance = (distance: number) =>
+    `${sprintf('%.2f', distance / 1000)} km`
