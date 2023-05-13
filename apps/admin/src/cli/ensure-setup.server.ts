972b2f18083c00c18d424c48ef81c354bbc25e69
Lencse <leventeloki@gmail.com>
2023-05-13 02:00:43 +0200

    .

--- apps/admin/src/cli/ensure-setup.server.ts
+++ apps/admin/src/cli/ensure-setup.server.ts
@@ -2,20 +2,17 @@ import { env } from 'node:process'
 import { writeFileSync, existsSync, statSync } from 'node:fs'
 
 export const ensureSetup = () => {
-    if (
-        !env['GCLOUD_RUNTIME_SERVICE_ACCOUNT'] ||
-        !env['GOOGLE_APPLICATION_CREDENTIALS'] ||
-        existsSync(env['GOOGLE_APPLICATION_CREDENTIALS'])
-    ) {
+    const encodedServiceAccount = env['GCLOUD_RUNTIME_SERVICE_ACCOUNT']
+    const credentialsFile = env['GOOGLE_APPLICATION_CREDENTIALS']
+    if (!encodedServiceAccount || !credentialsFile || existsSync(credentialsFile)) {
         return
     }
 
-    const serviceAccount = Buffer.from(env['GCLOUD_RUNTIME_SERVICE_ACCOUNT'], 'base64').toString(
-        'utf-8'
-    )
-    console.log({ serviceAccountLength: serviceAccount.length })
-    writeFileSync(env['GOOGLE_APPLICATION_CREDENTIALS'], serviceAccount.toString())
-    console.log(env['GOOGLE_APPLICATION_CREDENTIALS'], 'written')
-    const stats = statSync(env['GOOGLE_APPLICATION_CREDENTIALS'])
-    console.log('file size', stats.size)
+    const serviceAccount = Buffer.from(encodedServiceAccount, 'base64').toString('utf-8')
+    writeFileSync(credentialsFile, serviceAccount.toString())
+    const stats = statSync(credentialsFile)
+    console.info({
+        credentialsFile: 'written',
+        fileSize: stats.size,
+    })
 }
